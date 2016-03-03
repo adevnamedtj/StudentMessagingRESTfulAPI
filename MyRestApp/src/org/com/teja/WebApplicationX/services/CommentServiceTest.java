71415b312507a86f73398854bcfa39b25d9e63f8
TejaKalagara <TejaKalagara@192.168.2.3>
2016-03-02 21:40:09 -0600

    pom update

---
+++ MyRestApp/src/org/com/teja/WebApplicationX/services/CommentServiceTest.java
@@ -0,0 +1,21 @@
+package org.com.teja.WebApplicationX.services;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(PowerMockRunner.class)
+public class CommentServiceTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
